9b7e03cfa774f7aeaed97a4f4e8e13165fe4a0a9
KaloqnIliev <koko.681@abv.bg>
2023-11-22 14:52:56 +0200

    .

--- Bash/node_exporter.sh
+++ Bash/node_exporter.sh
@@ -1,90 +1,46 @@
 #!/bin/bash
 
-# Update system
-sudo apt-get update
+# Download Node Exporter
+NODE_EXPORTER_VERSION="1.7.0" 
+wget https://github.com/prometheus/node_exporter/releases/download/v${NODE_EXPORTER_VERSION}/node_exporter-${NODE_EXPORTER_VERSION}.linux-amd64.tar.gz
+tar xvf node_exporter-${NODE_EXPORTER_VERSION}.linux-amd64.tar.gz
+sudo mv node_exporter-${NODE_EXPORTER_VERSION}.linux-amd64/node_exporter /usr/local/bin/
 
-# Install necessary packages
-sudo apt-get install -y curl wget apt-transport-https software-properties-common
+# Create a user for Node Exporter
+sudo useradd --no-create-home --shell /bin/false node_exporter
 
-# Install Prometheus
-PROMETHEUS_VERSION="2.48.0"  # Replace with the latest version
-wget https://github.com/prometheus/prometheus/releases/download/v${PROMETHEUS_VERSION}/prometheus-${PROMETHEUS_VERSION}.linux-amd64.tar.gz
-tar xvf prometheus-${PROMETHEUS_VERSION}.linux-amd64.tar.gz
-sudo mv prometheus-${PROMETHEUS_VERSION}.linux-amd64/prometheus /usr/local/bin/
-sudo mv prometheus-${PROMETHEUS_VERSION}.linux-amd64/promtool /usr/local/bin/
-sudo mv prometheus-${PROMETHEUS_VERSION}.linux-amd64/prometheus.yml /etc/prometheus/
-sudo useradd --no-create-home --shell /bin/false prometheus
-sudo mkdir /etc/prometheus /var/lib/prometheus
-sudo cp -r prometheus-${PROMETHEUS_VERSION}.linux-amd64/consoles /etc/prometheus/
-sudo cp -r prometheus-${PROMETHEUS_VERSION}.linux-amd64/console_libraries /etc/prometheus/
-sudo chown -R prometheus:prometheus /etc/prometheus /var/lib/prometheus
+# Set permissions
+sudo chown node_exporter:node_exporter /usr/local/bin/node_exporter
 
-# Create Prometheus configuration
-cat <<EOF | sudo tee /etc/prometheus/prometheus.yml
-global:
-  scrape_interval: 15s  
+# Create a directory for Node Exporter
+sudo mkdir /etc/node_exporter
 
+# Create a configuration file for Node Exporter
+cat <<EOF | sudo tee /etc/node_exporter/node_exporter.yml
 scrape_configs:
   - job_name: 'node'
-    scrape_interval: 5s  
     static_configs:
-      - targets: ['localhost:9100']  
-    metrics_path: /metrics
-    params:
-      format: [node_memory_MemTotal,node_memory_MemFree,node_cpu_seconds_total,node_filesystem_avail_bytes,node_filesystem_size_bytes]
-  - job_name: 'prometheus'
-    scrape_interval: 10s  
-    static_configs:
-      - targets: ['localhost:9090']
+      - targets: ['localhost:9100']
 EOF
 
-# Create Prometheus service
-cat <<EOF | sudo tee /etc/systemd/system/prometheus.service
+# Create a service file for Node Exporter
+cat <<EOF | sudo tee /etc/systemd/system/node_exporter.service
 [Unit]
-Description=Prometheus
+Description=Node Exporter
 Wants=network-online.target
 After=network-online.target
 
 [Service]
-User=prometheus
-Group=prometheus
+User=node_exporter
+Group=node_exporter
 Type=simple
-ExecStart=/usr/local/bin/prometheus --config.file /etc/prometheus/prometheus.yml --storage.tsdb.path /var/lib/prometheus --web.console.templates=/etc/prometheus/consoles --web.console.libraries=/etc/prometheus/console_libraries
+ExecStart=/usr/local/bin/node_exporter 
 
 [Install]
 WantedBy=multi-user.target
 EOF
 
-# Reload systemd and start Prometheus
+# Reload systemd, enable and start the Node Exporter service
 sudo systemctl daemon-reload
-sudo systemctl enable prometheus
-sudo systemctl start prometheus
-
-# Install Grafana
-sudo apt-get update
-sudo apt-get install -y adduser libfontconfig1 musl
-wget https://dl.grafana.com/enterprise/release/grafana-enterprise_10.2.2_amd64.deb
-sudo dpkg -i grafana-enterprise_10.2.2_amd64.deb
-sudo systemctl daemon-reload
-sudo systemctl enable grafana-server
-sudo systemctl start grafana-server
-
-# Configure Grafana to work with Prometheus
-cat <<EOF | sudo tee -a /etc/grafana/grafana.ini
-[server]
-protocol = http
-http_port = 3000
-
-[datasource]
-type = prometheus
-url = http://localhost:9090
-access = proxy
-EOF
-
-# Restart Grafana
-sudo systemctl restart grafana-server
-
-# Clean up downloaded and extracted files
-cd ~
-sudo rm -rf prometheus-${PROMETHEUS_VERSION}.linux-amd64*
-sudo rm -f grafana-enterprise_10.2.2_amd64.deb
+sudo systemctl enable node_exporter
+sudo systemctl start node_exporter
