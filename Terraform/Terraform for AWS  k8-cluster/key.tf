ae51a4792d9c7868eb791bfc63734a531c263011
Kaloyan Iliev <118594294+KaloqnIliev@users.noreply.github.com>
2024-06-11 15:15:13 +0300

    .

--- Terraform/Terraform for AWS  k8-cluster/key.tf
+++ Terraform/Terraform for AWS  k8-cluster/key.tf
@@ -1,14 +1,9 @@
-resource "tls_private_key" "kube-key" {
+resource "tls_private_key" "ssh-key" {
   algorithm = "RSA"
   rsa_bits  = 4096
 }
 
-resource "aws_key_pair" "kube-key" {
+resource "aws_key_pair" "ssh-key" {
   key_name   = var.key_name
   public_key = tls_private_key.kube-key.public_key_openssh
-}
-
-resource "local_file" "private_key" {
-  content = tls_private_key.kube-key.private_key_pem
-  filename          = "${path.module}/kube-key.pem"
 }
