27124300dd2d49cd425625954a9ddab79dccc8c3
Kaloyan Iliev <118594294+KaloqnIliev@users.noreply.github.com>
2024-06-14 16:07:36 +0300

    .

--- Terraform/Terraform for AWS  k8-cluster/VPC.tf
+++ Terraform/Terraform for AWS  k8-cluster/VPC.tf
@@ -1,14 +1,29 @@
 resource "aws_vpc" "ubuntu-vpc" {
-  cidr_block = "10.0.0.0/16"
+  cidr_block = "172.16.0.0/16"  
 }
 
 resource "aws_subnet" "ubuntu-sub" {
   vpc_id     = aws_vpc.ubuntu-vpc.id
-  cidr_block = "10.0.1.0/24"
+  cidr_block = "172.16.1.0/24"  
+  map_public_ip_on_launch = true  
 }
 
 resource "aws_internet_gateway" "gateway" {
   vpc_id = aws_vpc.ubuntu-vpc.id
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.ubuntu-vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gateway.id
+  }
+}
+
+resource "aws_route_table_association" "public" {
+  subnet_id      = aws_subnet.ubuntu-sub.id
+  route_table_id = aws_route_table.public.id
 }
 
 resource "aws_security_group" "ubuntu-sg" {
