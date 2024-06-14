38b9969fc3b677c8833c353ce3187e110dac272f
Kaloyan Iliev <118594294+KaloqnIliev@users.noreply.github.com>
2024-06-14 17:03:11 +0300

    .

--- Bash/Monitoring/node_exporter.sh
+++ Bash/Monitoring/node_exporter.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# Download Node Exporter
-NODE_EXPORTER_VERSION="1.7.0" 
+# Node Exporter
+NODE_EXPORTER_VERSION="1.7.0"  # Replace with the desired version
 wget https://github.com/prometheus/node_exporter/releases/download/v${NODE_EXPORTER_VERSION}/node_exporter-${NODE_EXPORTER_VERSION}.linux-amd64.tar.gz
 tar xvf node_exporter-${NODE_EXPORTER_VERSION}.linux-amd64.tar.gz
 sudo mv node_exporter-${NODE_EXPORTER_VERSION}.linux-amd64/node_exporter /usr/local/bin/
@@ -18,9 +18,9 @@ sudo mkdir /etc/node_exporter
 # Create a configuration file for Node Exporter
 cat <<EOF | sudo tee /etc/node_exporter/node_exporter.yml
 scrape_configs:
-  - job_name: 'node'
-    static_configs:
-      - targets: ['localhost:9100']
+- job_name: 'node'
+static_configs:
+    - targets: ['localhost:9100']
 EOF
 
 # Create a service file for Node Exporter
@@ -29,18 +29,26 @@ cat <<EOF | sudo tee /etc/systemd/system/node_exporter.service
 Description=Node Exporter
 Wants=network-online.target
 After=network-online.target
-
+StartLimitIntervalSec=500
+StartLimitBurst=5
 [Service]
 User=node_exporter
 Group=node_exporter
 Type=simple
-ExecStart=/usr/local/bin/node_exporter 
-
+Restart=on-failure
+RestartSec=5s
+ExecStart=/usr/local/bin/node_exporter \
+--collector.logind
 [Install]
 WantedBy=multi-user.target
 EOF
 
 # Reload systemd, enable and start the Node Exporter service
 sudo systemctl daemon-reload
+sudo systemctl unmask node_exporter
 sudo systemctl enable node_exporter
 sudo systemctl start node_exporter
+
+# Clean up downloaded files
+rm node_exporter-${NODE_EXPORTER_VERSION}.linux-amd64.tar.gz
+rm -rf node_exporter-${NODE_EXPORTER_VERSION}.linux-amd64
